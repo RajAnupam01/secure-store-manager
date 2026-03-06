0cee9a6d36d057b8566a90a3de1cef6986af2d96
Anupam <anupam.r2301@gmail.com>
2026-03-06 11:25:22 +0530

    fixing minor bugs

--- app/Create.tsx
+++ app/Create.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import {  StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import React, { useState } from 'react'
 import * as SecureStore from 'expo-secure-store';
 
